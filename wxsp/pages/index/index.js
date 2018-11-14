4689e777761650fbfe027760cafbd4479ed740ae
Todd <136771184@qq.com>
2018-11-14 18:51:37 +0800

    小程序学习

--- wxsp/pages/index/index.js
+++ wxsp/pages/index/index.js
@@ -7,13 +7,43 @@ Page({
     motto: 'Hello World',
     userInfo: {},
     hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
+    canIUse: wx.canIUse('button.open-type.getUserInfo'),
+    inputValue: '',
+    product_name: ''
   },
   //事件处理函数
-  bindViewTap: function() {
-    wx.navigateTo({
-      url: '../logs/logs'
+  // bindViewTap: function() {
+  //   wx.navigateTo({
+  //     url: '../logs/logs'
+  //   })
+  // },
+  onShareAppMessage: function (res) {
+    var that = this;
+    return {
+      title: '你不懂的',
+      path: '/pages/index/index?id=' + that.data.scratchId,
+      success: function (res) {
+        // 转发成功
+        that.shareClick();
+      },
+      fail: function (res) {
+        // 转发失败
+      }
+    }
+  },
+  onPullDownRefresh: function () { // 下拉加载
+    setTimeout(function () {
+      wx.stopPullDownRefresh();
+      console.log(1);
+    }, 1000)
+  },
+  ProductNameInput: function (e) {
+    this.setData({
+      product_name: e.detail.value,
+      inputValue: '',
+      motto: e.detail.value // test
     })
+    console.log("input product name: ", e.detail.value)
   },
   onLoad: function () {
     if (app.globalData.userInfo) {
