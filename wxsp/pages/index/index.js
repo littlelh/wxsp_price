2f84599bbc45931972705b882082a208053816e0
lihan <lihan@jpush.cn>
2018-12-26 18:57:49 +0800

    更新小程序

--- wxsp/pages/index/index.js
+++ wxsp/pages/index/index.js
@@ -6,7 +6,9 @@ Page({
   data: {
     motto: 'Hello World!',
     inputValue: '',
-    product_name: ''
+    product_name: '',
+    goods_list: [{goods_price: 1999.9, goods_coupon: 300, goods_discount: '折扣', goods_name: '荣耀v10', goods_describe: '4GB+64GB', goods_img:'https://www.jiantong.xyz/loadimage'},
+      { goods_price: 6999, goods_coupon: 600, goods_discount: '折扣', goods_name: 'Iphone XR', goods_describe: '64GB', goods_img: 'https://www.jiantong.xyz/loadimage' }]
   },
   //事件处理函数
   // bindViewTap: function() {
